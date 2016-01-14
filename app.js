347b6c35cfe6aaf98cd1b636e93add1c88ca1d69
Jack Kinsey <jack.w.kinsey@gmail.com>
2016-01-13 23:00:26 -0600

    Final solution code clean up

--- app.js
+++ app.js
@@ -18,9 +18,10 @@
  */
 var CELL_SIZE = 15;
 
-// TODO: this should be variable (and set by the user).
 /** The frames per second in which the simulation should run. */
 var fps = 1;
+
+/** The speed of the simulation. */
 var speed = 1000/fps;
 
 /** Flag to determine if game is running, false by default. */
@@ -32,12 +33,15 @@ var canvas = document.getElementById('canvas');
 /** The 2D context of the canvas to handle the draw calls. */
 var context = canvas.getContext('2d');
 
+/** Reference to the board. */
+var board;
+
 /**
- * Model for Cell object.
+ * Model for cell object.
  *
  * @constructor
- * @param {Number} x - The x-coordinate of the cell in terms of the Board.
- * @param {Number} y - The y-coordinate of the cell in terms of the Board.
+ * @param {Number} x - The x-coordinate of the cell in terms of the board.
+ * @param {Number} y - The y-coordinate of the cell in terms of the board.
  */
 function Cell (x, y, color) {
     this.x = x;
@@ -48,28 +52,28 @@ function Cell (x, y, color) {
 }
 
 /**
- * Draws the Cell object on the canvas.
+ * Draws the cell object on the canvas.
  */
 Cell.prototype.draw = function () {
-    // Clear the rectangle that encloses this Cell (but making sure it
+    // Clear the rectangle that encloses this cell (but making sure it
     // is smaller than the grid lines so those don't get cleared).
     context.clearRect(this.x * CELL_SIZE + 1,
                       this.y * CELL_SIZE + 1,
                       CELL_SIZE - 2,
                       CELL_SIZE - 2 );
 
-    // Draw this Cell if it is alive.
+    // Draw this cell if it is alive.
     if (this.isAlive) {
-        // Set alpha value to the Cell's maturity value.
+        // Set alpha value to the cell's maturity value.
         context.globalAlpha = this.maturity;
 
-        // Color the Cell using its color value.
+        // Color the cell using its color value.
         context.fillStyle = this.color;
 
         // Begin the draw path
         context.beginPath();
 
-        // Draw an arc with the center point at the center of this Cell,
+        // Draw an arc with the center point at the center of this cell,
         // and a radius slightly smaller than the CELL_SIZE (so it fits
         // nicely in the grid).
         context.arc((this.x * CELL_SIZE) + CELL_SIZE / 2,
@@ -80,10 +84,10 @@ Cell.prototype.draw = function () {
         // Close the path to finish the circle.
         context.closePath();
 
-        // Fill the circle with the Cell's color.
+        // Fill the circle with the cell's color.
         context.fill();
 
-        // Set the styling & draw the outline of the Cell.
+        // Set the styling & draw the outline of the cell.
         context.lineWidth = 2;
         context.strokeStyle = 'black';
         context.stroke();
@@ -94,30 +98,37 @@ Cell.prototype.draw = function () {
 };
 
 /**
- * Toggle's the Cell object; killing it if it is
+ * Toggle's the cell object; killing it if it is
  * alive or resurrecting it if it is dead.
  */
 Cell.prototype.toggle = function () {
+    // Get the currently chosen color.
     var color = colorPalette.getColor();
 
+    // If the cell the user clicked is alive...
     if (this.isAlive) {
+        // ...and if it is the same color as the chosen color...
         if (this.color === color) {
+            // ...then kill it.
             this.kill();
         } else {
+            // Otherwise, change the color to the chosen color.
             this.color = color;
         }
     } else {
+        // Otherwise, bring the cell to life and make it the
+        // same color as the chosen color.
         this.isAlive = true;
         this.color = color;
     }
 
-    // Immediately draw this Cell instead of waiting on the Board
+    // Immediately draw this cell instead of waiting on the board
     // to do so, this helps the app feel more responsive to user input.
     this.draw();
 };
 
 /**
- * Kills the Cell object.
+ * Kills the cell object.
  */
 Cell.prototype.kill = function() {
     // Set alive status to false and reset the maturity value.
@@ -131,12 +142,12 @@ Cell.prototype.kill = function() {
 };
 
 /**
- * Model for Board object. The game's board in which the
- * Cell objects live.
+ * Model for board object. The game's board in which the
+ * cell objects live.
  *
  * @constructor
- * @param {Number} width - The width, in Cells, of the game board.
- * @param {Number} height - The height, in Cells, of the game board.
+ * @param {Number} width - The width, in cells, of the game board.
+ * @param {Number} height - The height, in cells, of the game board.
  */
 function Board(width, height) {
     this.width = width;
@@ -150,6 +161,7 @@ function Board(width, height) {
     context.canvas.width = this.width * CELL_SIZE;
     context.canvas.height = this.height * CELL_SIZE;
 
+    // Initialize the board.
     // Initialize the board with dead cells.
     for (var x = 0; x < this.width; x++) {
         var colCell = [];
@@ -166,8 +178,8 @@ function Board(width, height) {
 }
 
 /**
- * Clears the Board with dead cells by traversing it and
- * killing each Cell.
+ * Clears the board with dead cells by traversing it and
+ * killing each cell.
  */
 Board.prototype.clear = function () {
     console.log("clear board");
@@ -178,23 +190,27 @@ Board.prototype.clear = function () {
     }
 };
 
-// TODO: draw functions should have a reference to the context?
-// i.e. function(ctx)?
 /**
- * Draws the current state of the Board object by traversing
- * the Board and drawing each Cell object.
+ * Draws the current state of the board object by traversing
+ * the board and drawing each cell object.
  */
 Board.prototype.draw = function () {
-    var xPos;
-    var yPos;
+    // Width and Height of board in pixels.
     var widthPx = this.width * CELL_SIZE;
     var heightPx = this.height * CELL_SIZE;
+
+    // Clear the canvas.
     context.clearRect(0, 0, widthPx, heightPx);
-    //context.beginPath();
 
-    // TODO: move drawing code to two functions?
-    // drawCells and drawGridlines?
+    // Draw the cells and the grid overlay.
+    this.drawCells();
+    this.drawGrid(widthPx, heightPx);
+};
 
+/**
+ * Function to draw all of the board's cells.
+ */
+Board.prototype.drawCells = function () {
     // For every cell on the board...
     for (var x = 0; x < this.width; x++) {
         for (var y = 0; y < this.height; y++) {
@@ -202,22 +218,26 @@ Board.prototype.draw = function () {
             this.cells[x][y].draw();
         }
     }
+};
 
-    // TODO: draw grid once? Figure out a way to make that work?
-    for (var i = 0; i < this.width; i++) {
-        xPos = i * CELL_SIZE;
+Board.prototype.drawGrid = function (width, height) {
+    // Draw the vertical lines.
+    for (var x = 0; x < this.width; x++) {
+        var xPos = x * CELL_SIZE;
         context.beginPath();
         context.moveTo(xPos, 0);
-        context.lineTo(xPos, heightPx);
+        context.lineTo(xPos, height);
         context.closePath();
         context.strokeStyle = '#ddd';
         context.stroke();
     }
-    for (var j = 0; j < this.height; j++) {
-        yPos = j * CELL_SIZE;
+
+    // Draw the horizontal lines.
+    for (var y = 0; y < this.height; y++) {
+        var yPos = y * CELL_SIZE;
         context.beginPath();
         context.moveTo(0, yPos);
-        context.lineTo(widthPx, yPos);
+        context.lineTo(width, yPos);
         context.closePath();
         context.strokeStyle = '#ddd';
         context.stroke();
@@ -225,11 +245,20 @@ Board.prototype.draw = function () {
 };
 
 /**
- * Updates the state of each Cell object for the Board.
+ * Updates the state of each cell object for the board.
  */
 Board.prototype.update = function () {
     // Determine the state of the next generation of each
-    // Cell object on the Board.
+    // cell object on the board.
+    this.findNext();
+
+
+    // Use the state of the next generation to affect each
+    // cell object on the board.
+    this.applyNext();
+};
+
+Board.prototype.findNext = function () {
     for (var x = 0; x < this.width; x++) {
         for (var y = 0; y < this.height; y++) {
             this.next[x][y] = this.cells[x][y].isAlive &&
@@ -237,30 +266,30 @@ Board.prototype.update = function () {
                               this.getLivingNeighbors(x, y) === 3;
         }
     }
+};
 
-    // Use the state of the next generation to affect each
-    // Cell object on the Board.
+Board.prototype.applyNext = function () {
     for (var x = 0; x < this.width; x++) {
         for (var y = 0; y < this.height; y++) {
-            // The current Cell
+            // The current cell
             var cell = this.cells[x][y];
 
-            // The "alive" status of this Cell in the next generation.
+            // The "alive" status of this cell in the next generation.
             var aliveNextGen = this.next[x][y];
 
-            // If this Cell is currently alive and it is
+            // If this cell is currently alive and it is
             // still alive next generation
             if (cell.isAlive && aliveNextGen) {
-                // then increment this Cell's maturity if it isn't
+                // then increment this cell's maturity if it isn't
                 // already at the maximum of 1
                 if (cell.maturity < 1)
                     cell.maturity += 0.1;
             } else if (cell.isAlive && !aliveNextGen) {
-                // Otherwise, if this Cell is currently alive and
+                // Otherwise, if this cell is currently alive and
                 // it is not alive next generation, kill it.
                 cell.kill();
             } else if (!cell.isAlive && aliveNextGen) {
-                // Otherwise, if this Cell is currently dead and
+                // Otherwise, if this cell is currently dead and
                 // is is alive next generation, bring it to life;
                 cell.isAlive = true;
             }
@@ -269,12 +298,13 @@ Board.prototype.update = function () {
 };
 
 /**
- * Retrieves the number of living Cell objects surrounding
- * the Cell at the given location.
+ * Retrieves the number of living cell objects surrounding
+ * the cell at the given location, and applies a new color
+ * to the cell at the given location if appropriate.
  *
- * @param {Number} x - The x-coordinate of the Cell object in terms of the Board.
- * @param {Number} y - The y-coordinate of the Cell object in terms of the Board.
- * @returns {Number} The number of living neighbors the Cell has.
+ * @param {Number} x - The x-coordinate of the cell object in terms of the board.
+ * @param {Number} y - The y-coordinate of the cell object in terms of the board.
+ * @returns {Number} The number of living neighbors the cell has.
  */
 Board.prototype.getLivingNeighbors = function (x, y) {
     // A reference to the cells array of the board.
@@ -387,54 +417,35 @@ Board.prototype.getLivingNeighbors = function (x, y) {
 };
 
 /**
- * When a Cell on the board is clicked, toggle
- * the "alive" state of the Cell at the given
+ * When a cell on the board is clicked, toggle
+ * the "alive" state of the cell at the given
  * location on the board.
  *
- * @param {Number} x - The x-coordinate of the Cell in terms of the Board.
- * @param {Number} y - The y-coordinate of the Cell in terms of the Board.
+ * @param {Number} x - The x-coordinate of the cell in terms of the board.
+ * @param {Number} y - The y-coordinate of the cell in terms of the board.
  */
 Board.prototype.clickCell = function(x, y) {
     this.cells[x][y].toggle();
 };
 
-// TODO: allow user to set width and height params.
-// Create a new Board object.
-var board = new Board(50, 30);
-// Draw the initial state of the board (empty).
-// We do this to get our nice grid overlay drawn.
-board.draw();
-
-var gameLoop = function () {
-    if (running) {
-        board.update();
-        board.draw();
-    }
-};
-
-var interval = setInterval(gameLoop, speed);
-
-var modSpeed = function (newSpeed) {
-    clearInterval(interval);
-    interval = setInterval(gameLoop, newSpeed);
-};
-
-var fpsRange = document.getElementById('fps-slider');
-fpsRange.addEventListener('change', function() {
-    modSpeed(1000/fpsRange.value);
-});
 // Object to represent the Color Palette and have some
 // helper methods for setting and getting the color.
 var colorPalette = {
+    // Choose a color (highlight the chosen one).
     chooseColor: function(id) {
+        // Clear class style from current chosen color.
         var currentChosenColorElem = document.getElementsByClassName('chosen');
         if (currentChosenColorElem[0]) {
             currentChosenColorElem[0].className = 'color-picker';
         }
 
+        // Get the new chosen color and give it class 'chosen'
+        // to help it stand out as selected.
         var chosenColor = document.getElementById(id);
         chosenColor.className = chosenColor.className + ' chosen';
     },
+
+    // Finds the color selector with class 'chosen' and returns its value.
     getColor: function() {
         var currentChosenColorElem = document.getElementsByClassName('chosen');
         if (currentChosenColorElem[0]) {
@@ -442,8 +453,31 @@ var colorPalette = {
         }
     }
 };
-// Select the first color in the palette by default.
-document.getElementById('color1-btn').click();
+
+// Function to create a new board and draw it.
+init = function() {
+    board = new Board(
+        Math.round(document.getElementById('width').value),
+        Math.round(document.getElementById('height').value));
+
+    // Draw the initial state of the board (empty).
+    // We do this to get our nice grid overlay drawn.
+    board.draw();
+
+    // Select the first color in the palette by default.
+    document.getElementById('color1-btn').click();
+}();
+
+// Set up the game loop
+var gameLoop = function () {
+    if (running) {
+        board.update();
+        board.draw();
+    }
+};
+
+// Set up the interval for running the game loop.
+var interval = setInterval(gameLoop, speed);
 
 // INPUT -----------------------------------------------------------------------
 // Add event handlers for various hot keys the user can use
@@ -454,27 +488,39 @@ window.addEventListener('keydown', function(event){
     // But ignore input if user is typing in width or height of the board.
     if ((document.getElementById('width') !== document.activeElement) &&
         (document.getElementById('height') !== document.activeElement)) {
-        if (event.keyCode === 49 || event.keyCode === 97) {
-            document.getElementById('color1-btn').click();
-        } else if (event.keyCode === 50 || event.keyCode === 98) {
-            document.getElementById('color2-btn').click();
-        } else if (event.keyCode === 51 || event.keyCode === 99) {
-            document.getElementById('color3-btn').click();
-        } else if (event.keyCode === 52 || event.keyCode === 100) {
-            document.getElementById('color4-btn').click();
-        } else if (event.keyCode === 53 || event.keyCode === 101) {
-            document.getElementById('color5-btn').click();
-        } else if (event.keyCode === 54 || event.keyCode === 102) {
-            document.getElementById('color6-btn').click();
+        switch (event.keyCode) {
+            case 49:
+            case 97:
+                document.getElementById('color1-btn').click();
+                break;
+            case 50:
+            case 98:
+                document.getElementById('color2-btn').click();
+                break;
+            case 51:
+            case 99:
+                document.getElementById('color3-btn').click();
+                break;
+            case 52:
+            case 100:
+                document.getElementById('color4-btn').click();
+                break;
+            case 53:
+            case 101:
+                document.getElementById('color5-btn').click();
+                break;
+            case 54:
+            case 102:
+                document.getElementById('color6-btn').click();
         }
     }
 }, false);
 
 // Add event handler for the user clicking on the canvas (the board).
 canvas.addEventListener('mousedown', function(event){
-    // TODO: add compatibility for Firefox.
-    var x = event.x - canvas.offsetLeft;
-    var y = event.y - canvas.offsetTop;
+    var bounds = canvas.getBoundingClientRect();
+    var x = event.clientX - bounds.left;
+    var y = event.clientY - bounds.top;
 
     x /= CELL_SIZE;
     y /= CELL_SIZE;
@@ -484,6 +530,16 @@ canvas.addEventListener('mousedown', function(event){
 
     board.clickCell(x, y);
 }, false);
+
+// A reference to the fps slider control.
+var fpsRange = document.getElementById('fps-slider');
+
+// When the slider changes, modify the interval for the game
+// loop to increase or decrease the speed of the simulation.
+fpsRange.addEventListener('change', function() {
+    clearInterval(interval);
+    interval = setInterval(gameLoop, 1000 / fpsRange.value);
+});
 
 
 // Helper functions ------------------------------------------------------------
